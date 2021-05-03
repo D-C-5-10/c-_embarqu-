74a51ba1c09d663963760d1386960b7c3c4644b0
D.C.5.10 <dhiaeddine.chaabene@esprit.tn>
2021-05-04 01:48:05 +0200

    mise à jour du code

--- embed/main.cpp
+++ embed/main.cpp
@@ -1,3 +1,11 @@
+#include "uart.h"
+
 int main(void)
 {
+	uart_init();
+	while(1){
+		uart_write('Y');
+		for(int i=0;i<180000;++i){}
+	}
 }
+
--- embed/uart.cpp
+++ embed/uart.cpp
@@ -1,5 +1,27 @@
 #include "uart.h"
 void uart_init(void)
 {
- RCC->AHB1ENR |= 0x1;
+	/*******Initialisation*******/
+  RCC->AHB1ENR  = 0x1; //enable GPIOA clk
+	RCC->APB1ENR = 0x20000; //enable USART2 clk
+	/*******configuration*******/
+	GPIOA->MODER = 0x00A0; // seting pin 2 and 3 to alternate mode
+	GPIOA->AFR[0] |= 0x7700; //Alternate function for usart2 AF7 in AFRL
+	USART2->BRR = 0x0683;// 9600 Baud rate (default)
+	USART2->CR1 = 0x000C; // enable tx,Rx and 8 bit data 
+	USART2->CR2 =0x000;  //1 stop bit
+	USART2->CR3 = 0x000; // No flow control
+	USART2->CR1 |= 0x2000; // enable USART
 }
+int uart_write(int ch)
+{
+	while(!(USART2->SR & 0x0080)){} //wait until tx idle
+	USART2->DR = (ch & 0xFF);
+	return 1;
+}
+int uart_read(void)
+{
+	while(!(USART2->SR & 0x0020)){}
+  return USART2->DR ;
+}
+	
